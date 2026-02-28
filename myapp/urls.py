128b2a9c99556a32b70d227eedc4b857cfb816fb
Yussuf-2001 <yussufy315@gmail.com>
2026-02-28 16:59:21 +0300

    Remove BOM from myapp/urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,4 @@
-﻿﻿from django.conf import settings
+﻿from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path, re_path
 from django.views.static import serve
